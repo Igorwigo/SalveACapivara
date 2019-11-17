ed3e8b56846cbb9030deee5d3cd64f434c1a007c
jheymersonla <jheymerson.pmw50@gmail.com>
2019-11-16 22:33:04 -0300

    Adicionado background - MAIN

--- scr/scenes/main.js
+++ scr/scenes/main.js
@@ -4,9 +4,12 @@ mainScene.init = function () {
 };
 
 mainScene.preload = function () {
+    this.load.image('background', 'assets/images/background.png');
 };
 
 mainScene.create = function () {
+    let bg = this.add.sprite(0, 0, 'background');
+    bg.setOrigin(0, 0);
 };
 
 mainScene.update = function () {
