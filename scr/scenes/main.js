496692d2305d12336a7b61004cd5734e17935904
Igor Valadares Queiroz <igor.v.q@rede.ulbra.br>
2019-11-18 22:28:31 -0300

    modificação nos botões/capivara

--- scr/scenes/main.js
+++ scr/scenes/main.js
@@ -7,46 +7,73 @@ mainScene.preload = function () {
     this.load.image('background', 'assets/images/background.png');
     this.load.spritesheet('capivara', 'assets/images/capivara.png', { frameWidth: 75, frameHeight: 48 });
     this.load.image('botao','assets/images/botao.png')
-
+    this.load.image('triangulo','assets/images/triangulo.png')
+    this.load.image('quadrado','assets/images/quadrado.png')
+    this.load.image('bola','assets/images/bola.png')
 };
 
 mainScene.create = function () {
 
 
 
-    let bg = this.add.sprite(0, 0, 'background');
+    let bg = this.add.image(0, 0, 'background');
     bg.setOrigin(0, 0);
 
-    player = this.physics.add.sprite(150, 250, 'capivara');
-    player.setScale(0.7);
+    capivara = this.physics.add.sprite(30, 250, 'capivara');
+    capivara.setScale(0.7);
 
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('capivara', { start: 0, end: 2 }),
-        frameRate: 10,
-        repeat: -1
-    });
+//
+//    this.anims.create({
+//        key: 'left',
+//        frames: this.anims.generateFrameNumbers('capivara', { start: 0, end: 2 }),
+  //      frameRate: 10,
+    //    repeat: -1
+ //   });
 
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('capivara', { start: 3, end: 5 }),
-        frameRate: 10,
-        repeat: -1
-    });
+  //  this.anims.create({
+    //    key: 'right',
+      //  frames: this.anims.generateFrameNumbers('capivara', { start: 3, end: 5 }),
+        //frameRate: 10,
+        //repeat: -1
+   // });
+
+ //   this.anims.create({
+   //     key: 'turn',
+     //   frames: [{ key: 'capivara', frame: 3 }],
+       // frameRate: 10,
+   // });
+//
+    
+
+
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    var botao1 = this.add.image(219, 315, 'botao').setInteractive();
-    botao1.on('pointerdown', function (event) {
-     console.log("butao1")   
+    var img_triangulo = this.add.image(219,40,'triangulo');
+
+    var img_bola = this.add.image(293,40,'bola');
+    
+    var img_quadrado = this.add.image(367,40,'quadrado');
+
+
+
+    var botao_triangulo = this.add.image(219, 315, 'triangulo').setInteractive();
+    botao_triangulo.on('pointerdown', function (event) {
+        if(img_triangulo.texture.key=="triangulo"){
+        img_triangulo.setVisible(false);
+        console.log("Triângulo")
+   
+    }
     });
-    var botao2 = this.add.image(293, 315, 'botao').setInteractive();
-    botao2.on('pointerdown', function (event) {
-     console.log("butao2")   
+    var botao_bola = this.add.image(293, 315, 'bola').setInteractive();
+    botao_bola.on('pointerdown', function (event) {
+     console.log("bola")  
+     img_bola.setVisible(false); 
     });
-    var botao3 = this.add.image(367, 315, 'botao').setInteractive();
-    botao3.on('pointerdown', function (event) {
-     console.log("butao3")   
+    var botao_quadrado = this.add.image(367, 315, 'quadrado').setInteractive();
+    botao_quadrado.on('pointerdown', function (event) {
+        img_quadrado.setVisible(false)
+        console.log("quadro")   
     });
 
 
@@ -54,14 +81,24 @@ mainScene.create = function () {
 };
 
 mainScene.update = function () {
-    
-    if (cursors.left.isDown) {
-        player.setVelocityX(-160);
-        player.anims.play('left', true);
+    for (let i=0;i<1;i++){
+        capivara.setVelocityX(1)
+
     }
-    else if (cursors.right.isDown) {
-        player.setVelocityX(160);
-        player.anims.play('right', true);
-    }
+
+
+
+//    if (cursors.left.isDown) {
+//        capivara.setVelocityX(-160);
+//        capivara.anims.play('left', true);
+    //}
+//    else if (cursors.right.isDown) {
+ //       capivara.setVelocityX(160);
+   //     capivara.anims.play('right', true);
+    //}
+    //else {
+      //  capivara.setVelocityX(0);
+        //capivara.anims.play('turn');
+    //}
 
 };
