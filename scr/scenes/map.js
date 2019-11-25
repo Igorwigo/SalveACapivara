90d19b4688c62d4094f6b4fdd8422a4fa4a6dd26
Your Name <you@example.com>
2019-11-25 11:49:46 -0300

    logica no mapa feita

--- scr/scenes/map.js
+++ scr/scenes/map.js
@@ -13,24 +13,37 @@ mapScene.create = function () {
     let bg = this.add.image(0, 0, 'mapa');
     bg.setOrigin(0, 0);
     trocaSceneFase1=false;
+    trocaSceneFase2=false;
 
    /*----------------------------BOTAO_PLAY---------------------------------- */
 
-   var botao_quadrado = this.add.image(320, 180, 'play').setInteractive();
-   botao_quadrado.on('pointerdown', function (event) {
+   var botao_fase1 = this.add.image(100, 180, 'play').setScale(0.5).setInteractive();
+   botao_fase1.on('pointerdown', function (event) {
     trocaSceneFase1=true;
     console.log("play")
      });
    /*-------------------------------------------------------------- */
+   var botao_fase2 = this.add.image(500, 180, 'play').setScale(0.5).setAlpha(0.5);
+   
+   if(localStorage.getItem('placar1')==3){   
+    botao_fase2.setInteractive().setAlpha();
+}
+   
+   botao_fase2.on('pointerdown', function (event) {
+        trocaSceneFase2=true;
+
+    
 
 
+    
+    
+    console.log("play")
+  });
 
+   if(localStorage.getItem('placar1')==3){   
+    
 
-   //localStorage.getItem('placar1')==3
-
-
-
-
+}
 
 };
 
@@ -40,4 +53,7 @@ mapScene.update = function () {
         this.scene.start('main');
     }
 
+    if(trocaSceneFase2==true){
+        this.scene.start('fase2');
+    }
 };
