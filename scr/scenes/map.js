dd0feb7b4c8a55811884afb20cfa86f8ddc71e5f
jheymersonla <jheymerson.pmw50@gmail.com>
2019-11-25 17:21:39 -0300

    Imagens Alteradas v.3.0

--- scr/scenes/map.js
+++ scr/scenes/map.js
@@ -5,8 +5,8 @@ mapScene.init = function () {
 
 mapScene.preload = function () {
     this.load.image('mapa', 'assets/images/mapa.png');
-    this.load.image('play', 'assets/images/play.png');
-    this.load.image('quadrado', 'assets/images/quadrado.png');
+    this.load.image('fase1-icon', 'assets/images/fase1-icon.png');
+    this.load.image('fase2-icon', 'assets/images/fase2-icon.png');
 
 };
 
@@ -16,14 +16,14 @@ mapScene.create = function () {
     trocaSceneFase1=false;
     trocaSceneFase2=false;
 
-   /*----------------------------BOTAO_PLAY---------------------------------- */
+   /*----------------------------BOTAO_fase1-icon---------------------------------- */
 
-   var botao_fase1 = this.add.image(100, 180, 'play').setScale(0.5).setInteractive();
+   var botao_fase1 = this.add.image(166, 267, 'fase1-icon').setScale(0.5).setInteractive();
    botao_fase1.on('pointerdown', function (event) {
     trocaSceneFase1=true;
      });
    /*-------------------------------------------------------------- */
-  var botao_fase2 = this.add.image(500, 180, 'quadrado').setScale(0.5).setAlpha(0.5);
+  var botao_fase2 = this.add.image(91, 235, 'fase2-icon').setScale(0.5).setAlpha(0.5);
    if(localStorage.getItem('placar1')==3){   
     botao_fase2.setInteractive().setAlpha();
     botao_fase2.on('pointerdown', function (event) {
